1359db4a7188e343ab7185bf178107c4b74d23f0
wangweiye <wwyknight@163.com>
2020-10-06 17:27:14 +0800

    fix

--- stream/stream-rabbit-customer/src/main/java/kite/springcloud/stream/rabbit/customer/service/LogMessageListener.java
+++ stream/stream-rabbit-customer/src/main/java/kite/springcloud/stream/rabbit/customer/service/LogMessageListener.java
@@ -16,18 +16,26 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class LogMessageListener {
+    int retryCount = 1;
 
     /**
      * 通过 MyProcessor.MESSAGE_INPUT 接收消息
      * 然后通过 SendTo 将处理后的消息发送到 MyProcessor.LOG_FORMAT_OUTPUT
+     *
+     * 当消费失败时，默认的策略是重试。当重试成功了，则不会抛出异常；当重试失败，抛出异常，消费结束。
      * @param message
      * @return
      */
     @StreamListener(MyProcessor.MESSAGE_INPUT)
     @SendTo(MyProcessor.LOG_FORMAT_OUTPUT)
     public String processLogMessage(String message) {
-        log.info("接收到原始消息：" + message);
-        return "「" + message +"」";
+        if (retryCount == 3) {
+            log.info(String.format("重试次数：%d, 接收到原始消息：%s", retryCount, message));
+            return "「" + message + "」";
+        } else {
+            retryCount++;
+            throw new RuntimeException("消费失败!");
+        }
     }
 
 //    @StreamListener(MyProcessor.MESSAGE_INPUT)
@@ -41,6 +49,7 @@ public class LogMessageListener {
      * 接收来自 MyProcessor.LOG_FORMAT_INPUT 的消息
      * 也就是加工后的消息，也就是通过上面的 SendTo 发送来的
      * 因为 MyProcessor.LOG_FORMAT_OUTPUT 和 MyProcessor.LOG_FORMAT_INPUT 是指向同一 exchange
+     *
      * @param message
      */
     @StreamListener(MyProcessor.LOG_FORMAT_INPUT)
