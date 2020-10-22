879c23ae012504b4b6b8ee7e02fa2d111d55ebfb
wangweiye <wwyknight@163.com>
2020-10-22 10:45:23 +0800

    fix

--- consul/consul-provider/src/main/java/kite/springcloud/consul/provider/controller/HelloController.java
+++ consul/consul-provider/src/main/java/kite/springcloud/consul/provider/controller/HelloController.java
@@ -4,11 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
