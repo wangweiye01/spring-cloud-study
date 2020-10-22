0fe5a37464da55f12267c087d672f655f9e3bb21
wangweiye <wwyknight@163.com>
2020-10-22 08:59:11 +0800

    fix

--- consul/consul-oauth-client/src/main/java/kite/springcloud/consul/oauth/client/config/ResourceServerConfig.java
+++ consul/consul-oauth-client/src/main/java/kite/springcloud/consul/oauth/client/config/ResourceServerConfig.java
@@ -3,7 +3,9 @@ package kite.springcloud.consul.oauth.client.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -44,4 +46,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         resources.tokenStore(jwtTokenStore);
     }
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin().and().authorizeRequests().antMatchers(HttpMethod.POST, "/actuator/health").permitAll();
+    }
 }
