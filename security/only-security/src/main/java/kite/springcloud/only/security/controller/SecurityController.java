7ada1eb3a013ef5fce6194bcf1b3f92f45deab03
wangweiye <wwyknight@163.com>
2020-10-24 14:00:06 +0800

    fix

--- security/only-security/src/main/java/kite/springcloud/only/security/controller/SecurityController.java
+++ security/only-security/src/main/java/kite/springcloud/only/security/controller/SecurityController.java
@@ -1,10 +1,6 @@
 package kite.springcloud.only.security.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,10 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "security")
 public class SecurityController {
-
     @GetMapping(value = "test")
     //@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    public String runSecurity(){
+    public String runSecurity() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         log.info(authentication.getName());
         return "hello security";
@@ -32,10 +27,7 @@ public class SecurityController {
 
     @GetMapping(value = "test2")
     //@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    public String runSecurity2(){
+    public String runSecurity2() {
         return "hello security2";
     }
-
-
-
 }
